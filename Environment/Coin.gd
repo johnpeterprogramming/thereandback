d581b283d70fac22cad81822faffe67755132ce8
Snowyboi <halfoburrito@gmail.com>
2020-09-21 17:02:11 +0200

    updated coins

--- Environment/Coin.gd
+++ Environment/Coin.gd
@@ -1,7 +1,11 @@
 extends Area2D
 
-
+var deathScene = preload("res://Environment/CoinTaken.tscn")
 
 
 func _on_Coin_body_entered(body):
-	pass # Replace with function body.
+	Singelton.coins += 1
+	var d = deathScene.instance()
+	d.global_position = global_position
+	get_parent().add_child(d)
+	queue_free()
